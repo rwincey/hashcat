4ab9b3f204f438c5b73b703b25ea3d616a423f25
qasikfwn <51647241+qasikfwn@users.noreply.github.com>
2024-02-24 18:27:20 +1100

    Fix seed length in ST_HASH

--- src/modules/module_90000.c
+++ src/modules/module_90000.c
@@ -25,7 +25,7 @@ static const u64   OPTS_TYPE      = OPTS_TYPE_STOCK_MODULE
                                   | OPTS_TYPE_SUGGEST_KG;
 static const u32   SALT_TYPE      = SALT_TYPE_EMBEDDED;
 static const char *ST_PASS        = "hashcat";
-static const char *ST_HASH        = "73f8142b4326d36a:0";
+static const char *ST_HASH        = "73f8142b4326d36a:00000000";
 
 u32         module_attack_exec    (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return ATTACK_EXEC;     }
 u32         module_dgst_pos0      (MAYBE_UNUSED const hashconfig_t *hashconfig, MAYBE_UNUSED const user_options_t *user_options, MAYBE_UNUSED const user_options_extra_t *user_options_extra) { return DGST_POS0;       }

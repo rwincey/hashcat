9d79fce8fe4bf94a004d550c202266cdf4cf37c2
Jens Steube <jens.steube@gmail.com>
2025-07-26 10:38:45 +0200

    Fix RC4 primitives bug on GPUs with thread size != 32
    Fixed a bug in the RC4 crypto primitives that can cause false
    negatives on GPUs which do not result in a thread size of 32 in
    module_jit_build_options(). This typically affects Intel GPUs
    since those often have a native thread size of 8, but not NVIDIA
    or AMD.
    
    The problem was in the key lookup logic in inc_cipher_rc4.cl. It was
    written in a way that assumes 32 threads are always running:
    
    If the kernel is launched with only 8 threads, this macro can result
    in out-of-bound reads and writes.
    
    To fix this, we now enforce that all kernels using the RC4 primitives
    are launched with 32 threads (on GPUs).
    
    The following hash modes were updated to include this fix:
    
    7500, 9700, 9710, 9720, 9800, 9810, 9820, 10400, 10410, 10420, 10500,
    10510, 13100, 18200, 25400, 33500, 33501, and 33502.

--- src/modules/module_10400.c
+++ src/modules/module_10400.c
@@ -88,33 +88,14 @@ char *module_jit_build_options (MAYBE_UNUSED const hashconfig_t *hashconfig, MAY
 {
   char *jit_build_options = NULL;
 
-  u32 native_threads = 0;
+  // We must override whatever tuningdb entry or -T value was set by the user with this
+  // That's because of RC code in inc_cipher_rc4.cl has this for GPU (different on CPU):
+  // #define KEY32(t,k) (((k) * 32) + ((t) & 31) + (((t) / 32) * 2048))
+  // #define KEY8(t,k) (((k) & 3) + (((k) / 4) * 128) + (((t) & 31) * 4) + (((t) / 32) * 8192))
 
-  if (device_param->opencl_device_type & CL_DEVICE_TYPE_CPU)
-  {
-    native_threads = 1;
-  }
-  else if (device_param->opencl_device_type & CL_DEVICE_TYPE_GPU)
-  {
-    #if defined (__APPLE__)
+  u32 native_threads = (device_param->opencl_device_type & CL_DEVICE_TYPE_CPU) ? 1 : 32;
 
-    native_threads = 32;
-
-    #else
-
-    if (device_param->device_local_mem_size < 49152)
-    {
-      native_threads = MIN (device_param->kernel_preferred_wgs_multiple, 32); // We can't just set 32, because Intel GPU need 8
-    }
-    else
-    {
-      native_threads = device_param->kernel_preferred_wgs_multiple;
-    }
-
-    #endif
-  }
-
-  hc_asprintf (&jit_build_options, "-D FIXED_LOCAL_SIZE=%u -D _unroll", native_threads);
+  hc_asprintf (&jit_build_options, "-D FIXED_LOCAL_SIZE=%u", native_threads);
 
   return jit_build_options;
 }

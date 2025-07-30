ec23a37963dd490b22cbc31f37b0628f92bb44b5
fse-a <p.barens@nfi.nl>
2025-07-30 16:21:33 +0200

    Merged hash extraction script for LUKSv2 into existing Python script for LUKS.

--- tools/luks2hashcat.py
+++ tools/luks2hashcat.py
@@ -6,11 +6,13 @@
 #
 
 from argparse import ArgumentParser
+from base64 import b64decode
 from collections import namedtuple
 from dataclasses import dataclass
 from os import SEEK_SET
 from struct import Struct
 from typing import List
+import json
 
 try:
     from enum import auto, IntEnum, StrEnum
@@ -301,6 +303,65 @@ def extract_version1(file):
         print(hash)
 
 
+@dataclass(init=False)
+class HeaderVersion2:
+    VERSION = 0x0002
+
+
+def extract_version2(file):
+    file.seek(0x1000)
+    json_data = file.read(32768)
+    json_end = json_data.find(b"\x00")
+    json_header = json.loads(json_data[:json_end])
+
+    # print("\nWe got the following large json header:\n")
+    # print(json.dumps(json_header, indent=4))
+
+    keyslot_offset = int(json_header['keyslots']['0']['area']['offset'])
+    keyslot_size = int(json_header['keyslots']['0']['area']['key_size'])
+    encryption = json_header['keyslots']['0']['area']['encryption']
+
+    (cipher_type, cipher_mode) = tuple(encryption.split("-", 1))
+
+    file.seek(keyslot_offset)
+    keyslot_encrypted_data = file.read(256000) #don't yet know how to calculate this length exactly
+
+    blocknumbers = int(json_header['keyslots']['0']['af']['stripes'])
+    hash_mode = json_header['keyslots']['0']['af']['hash']
+
+    # Include first sector of the segment for entropy check
+    segment_offset = int(json_header['segments']['0']['offset'])
+    file.seek(segment_offset)
+    first_sector = file.read(512)
+
+    kdf = json_header['keyslots']['0']['kdf']['type']
+    time = int(json_header['keyslots']['0']['kdf']['time'])
+    memory = int(json_header['keyslots']['0']['kdf']['memory'])
+    cpus = int(json_header['keyslots']['0']['kdf']['cpus'])
+    salt = b64decode(json_header['keyslots']['0']['kdf']['salt'])
+
+    key_size = int(json_header['keyslots']['0']['key_size']) * 8
+
+    hash = SIGNATURE + "$".join(
+        map(
+            str,
+            [
+                HeaderVersion2.VERSION,
+                kdf,
+                hash_mode,
+                cipher_type,
+                cipher_mode,
+                key_size,
+                f"m={memory},t={time},p={cpus}",
+                salt.hex(),
+                keyslot_encrypted_data.hex(),
+                first_sector.hex(),
+            ],
+        )
+    )
+    print(hash)
+
+
 # main
 
 
@@ -338,6 +399,7 @@ if __name__ == "__main__":
             try:
                 mapping = {
                     HeaderVersion1.VERSION: extract_version1,
+                    HeaderVersion2.VERSION: extract_version2,
                 }
                 extract = mapping[header.version]
                 extract(file)
